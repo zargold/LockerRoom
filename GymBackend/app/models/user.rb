81f2a820c0f0f06d79db7b579e1d38ca742d7ea3
Raz <razgoldin@gmail.com>
2015-05-26 16:22:32 -0400

    adding administrative pages and setting up user controller

--- GymBackend/app/models/user.rb
+++ GymBackend/app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+  before_save {self.email = email.downcase}
+  VALIDEMAIL = /\A[\w+\-.]+@[a-z\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+
+  validates :email,
+  presence: true, 
+  length: { minimum: 5, maximum: 200},
+  format: { with: VALIDEMAIL },
+  uniqueness: { case_sensitive: false}
+
+  validates :password, length: {
+    minimum: 5, maximum: 200 }, 
+    presence: true
+
+  has_secure_password
+
 end
