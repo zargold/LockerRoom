52ab31fd6ad51041b23267eb98ecf8b2a2c4f379
John Costanza <jcostanza8@yahoo.com>
2015-05-27 16:09:52 -0400

    all exercises

--- GymBackend/app/models/exercise.rb
+++ GymBackend/app/models/exercise.rb
@@ -1,3 +1,11 @@
 class Exercise < ActiveRecord::Base
 	belongs_to :goals
+
+	# def self.search(search)
+	# 	if search
+ #    		find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+ #  		else
+ #    		find(:all)
+ #  		end
+	# end
 end
--- GymBackend/db/seeds.rb
+++ GymBackend/db/seeds.rb
@@ -5,7 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'httparty'
 
+response = HTTParty.get('https://wger.de/api/v2/exercise/?format=json')
+
+response["results"].each do |exercise|
+  Exercise.create(name: exercise["name"], description: exercise["description"])
+end
 
 Goal.create(exercise_id: 1, reps: 12, weight: 50, user_id: 1)
-Exercise.create(name: "Curl", description: "Stand up straight with a dumbbell in each hand. Curl the the weights while contracting your biceps.")
